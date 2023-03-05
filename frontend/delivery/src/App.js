6956d8a977c6a41b73772dad71e99ea622133b1e
lorenzzz0 <lorenzzz0@icloud.com>
2023-03-05 02:43:39 -0600

    Agregar funcionalidad de autocompletado en el Frontend

--- frontend/delivery/src/App.js
+++ frontend/delivery/src/App.js
@@ -1,19 +1,37 @@
 import './App.css';
 
 
-const keyUpHandler = async(event) => {
+const keyUpHandlerCp = async(event) => {
+  const autoCompeltePrefix = (event.target.id === 'rCp') ? 'r' : 'c';
+  console.log({autoCompeltePrefix});
   const response = await fetch('https://gxicm35je9.execute-api.us-east-1.amazonaws.com/prod/cp', {
     method: 'POST',
-    mode: 'no-cors',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      cp: event.target.value,
+      cp: event.target.value.replaceAll(/\s/g,''),
     }),
   });
-  console.log(response);
+
+  let jsonResponse
+  try{
+    jsonResponse = await response.json();
+  }catch(e){
+    alert('Error al obtener la información');
+    console.log(e);
+  }
+  if(jsonResponse.length > 0){
+    const address = jsonResponse[0];
+    document.getElementById(autoCompeltePrefix +'Estado').value = '3';
+    document.getElementById(autoCompeltePrefix +'Ciudad').value = address.ciudad;
+    document.getElementById(autoCompeltePrefix +'Colonia').value = address.colonia;
+  }else{
+    document.getElementById(autoCompeltePrefix +'Ciudad').value = '';
+    document.getElementById(autoCompeltePrefix +'Colonia').value = '';
+    document.getElementById(autoCompeltePrefix +'Estado').value = '';
+  }
+  console.log(jsonResponse.length);
 };
 
 
@@ -29,7 +47,7 @@ function App() {
                 <input name="rNombre" type="text" id="rNombre" placeholder="Nombre"/>
               </div>
               <div className="content-input">
-                <input name="rCp" id="rCp" type="text" onKeyUp={keyUpHandler}  placeholder="Codigo postal"/>
+                <input name="rCp" id="rCp" type="text" onKeyUp={keyUpHandlerCp}  placeholder="Codigo postal"/>
               </div>
               <div className="content-input">
                 <select name="rEstado" id="rEstado">
@@ -62,13 +80,13 @@ function App() {
           <fieldset className="content-destination">
               <p>Consignatario</p>
               <div className="content-input">
-                <input name="rNombre" type="text" id="rNombre" placeholder="Nombre"/>
+                <input name="cNombre" type="text" id="cNombre" placeholder="Nombre"/>
               </div>
               <div className="content-input">
-                <input name="rCp" id="rCp" type="text" placeholder="Codigo postal"/>
+                <input name="cCp" id="cCp" type="text" placeholder="Codigo postal" onKeyUp={keyUpHandlerCp}/>
               </div>
               <div className="content-input">
-                <select name="rEstado" id="rEstado">
+                <select name="cEstado" id="cEstado">
                     <option value="">Estado</option>
                     <option value="1">Aguascalientes</option>
                     <option value="2">Baja California</option>
@@ -76,16 +94,16 @@ function App() {
                 </select>
               </div>
               <div className="content-input">
-                <input name="rCiudad" id="rCiudad" type="text" placeholder="Ciudad"/>
+                <input name="cCiudad" id="cCiudad" type="text" placeholder="Ciudad"/>
               </div>
               <div className="content-input">
-                <input name="rColonia" id="rColonia" type="text" placeholder="Colonia"/>
+                <input name="cColonia" id="cColonia" type="text" placeholder="Colonia"/>
               </div>
               <div className="content-input">
-                <input name="rCalle" id="rCalle" type="text" placeholder="Calle"/>
+                <input name="cCalle" id="cCalle" type="text" placeholder="Calle"/>
               </div>
               <div className="content-input">
-                <input name="rNumero" id="rNumero" type="text" placeholder="Número"/>
+                <input name="cNumero" id="cNumero" type="text" placeholder="Número"/>
               </div>
           </fieldset>
         </div>
